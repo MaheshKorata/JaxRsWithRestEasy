390cf6870f055c72e78138a7b6e67cce52a93462
MaheshKorata <maheshbrahma.k@gmail.com>
2023-12-13 00:02:36 +0530

    JaxRsWithRestEasy

---
+++ JaxRsWithRestEasy/src/main/java/com/services/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.services.entity.Employee;
+
+@Path("/employeeService")
+@Produces(MediaType.APPLICATION_JSON)
+public class EmployeeService {
+
+	@GET
+	public Employee getEmployee() {
+		System.out.println("--------");
+		return new Employee();
+	}
+
+}
