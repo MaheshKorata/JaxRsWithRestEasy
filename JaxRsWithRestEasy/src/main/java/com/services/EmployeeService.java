80013d498c5c5f1b543b9ed9fbc99c6b7233d265
MaheshKorata <maheshbrahma.k@gmail.com>
2023-12-13 00:19:47 +0530

    services added

--- JaxRsWithRestEasy/src/main/java/com/services/EmployeeService.java
+++ JaxRsWithRestEasy/src/main/java/com/services/EmployeeService.java
@@ -1,20 +1,44 @@
 package com.services;
 
+import java.io.IOException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.codehaus.jackson.map.ObjectMapper;
+
 import com.services.entity.Employee;
 
 @Path("/employeeService")
-@Produces(MediaType.APPLICATION_JSON)
 public class EmployeeService {
 
 	@GET
+	@Path("/returnEmployeeObj")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Employee getEmployee() {
+
 		System.out.println("--------");
+
 		return new Employee();
+	}
+
+	@GET
+	@Path("/returnJsonAsString")
+	public String getEmployeeAsString() {
+
+		System.out.println("--------");
+
+		ObjectMapper map = new ObjectMapper();
+		String value = null;
+		try {
+			value = map.writeValueAsString(new Employee());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return value;
 	}
 
 }
